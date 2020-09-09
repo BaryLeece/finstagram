87ec75466d4182b1ffee525e0e08ab5dff10b4f3
BaryLeece <b.leece1234@gmail.com>
2020-09-09 03:27:44 +0000

    it didnt break this time

--- app/actions.rb
+++ app/actions.rb
@@ -7,7 +7,7 @@ def humanized_time_ago(humanized_time_ago)
 end
 
 get '/' do
-finstagram_post_shark = {
+@finstagram_post_shark = {
     username: "sharky_j",
     avatar_url: "http://naserca.com/images/sharky_j.jpg",
     photo_url: "http://naserca.com/images/shark.jpg",
@@ -46,11 +46,6 @@ finstagram_post_shark = {
     }]
   }
 
-  get '/' do
-  # your previous finstagram post code is up here
-  # add the following @finstagram_posts line
-
-  @finstagram_posts = [@finstagram_post_shark, @finstagram_post_whale, @finstagram_post_marlin]
-
+@finstagram_posts = FinstagramPost.order(created_at: :desc)
   erb(:index)
 end
